c4270089c3d7c7ba08aed5d01e98162c87d86874
Lawrence <lawrence.nno@gmail.com>
2022-09-08 07:59:43 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: 0 is returned if the operation is successful
+ */
+int main ()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
