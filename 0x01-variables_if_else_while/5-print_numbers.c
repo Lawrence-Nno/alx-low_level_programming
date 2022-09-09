aa2e6b284aa1728fbf3718374a92382ee6d9f706
Lawrence <lawrence.nno@gmail.com>
2022-09-09 10:24:34 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: O is returned
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
