fbb30561ac658daa4298f0c275543ddc095dc5b2
Lawrence <lawrence.nno@gmail.com>
2022-09-09 08:09:11 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * No return for this proggram
+ */
+
+int main(void)
+{
+	char x;
+	char Y;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (Y = 'A'; Y <= 'Z'; Y++)
+		putchar(Y);
+	putchar('\n')
+}
