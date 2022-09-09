b7f6a1cd2d4fbc2c95b63c6ea0ec929bbe83b6de
Lawrence <lawrence.nno@gmail.com>
2022-09-09 10:05:52 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'e' && n != 'q')
+			putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
