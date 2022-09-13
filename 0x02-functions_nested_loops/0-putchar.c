701497b90d3b65f24004f08362ea0e41bd2f8943
Lawrence <lawrence.nno@gmail.com>
2022-09-13 07:53:39 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This the main function
+ *
+ * Return: This program returns 0
+ */
+int main(void)
+{
+	char x;
+
+	x = '_putchar';
+	if (x = '_putchar')
+		putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+I will update this readme with the proper descriptions of this dir whem I'm done with this project
