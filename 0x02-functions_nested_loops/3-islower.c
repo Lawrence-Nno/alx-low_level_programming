732a4f52aea5357f143e9b4269b2754f04a68b33
Lawrence <lawrence.nno@gmail.com>
2022-09-13 14:02:36 -0700

    Task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,15 @@
 
 /**
  * _islower - Checks for lower case letters
+ * c - Is the input character to check
  * 
  * Return: Returns 1 if lower case and 0 if otherwise
  */
 
 int _islower(int c)
 {
-	for (c = 'a'; c <= 'z'; c++)
+	char c;
+	for (c = a; c <= z; c++)
 		return (1);
 	return (0);
 }
