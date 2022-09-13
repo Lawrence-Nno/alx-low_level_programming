b99c300136721ae83d747326c3a8c373d431ea2a
Lawrence <lawrence.nno@gmail.com>
2022-09-13 15:38:33 -0700

    Task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - This function prints the last digit of a number
+ *@num: The number to be checked
+ * :Return: Returns 0
+ */
+
+int print_last_digit(int num)
+{
+	int x = (num % 10);
+
+	return (x);
+}
