423bf8c62f23066644c8dd8ad8da7030154f973a
Lawrence <lawrence.nno@gmail.com>
2022-09-13 11:02:22 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * main - This the main function
@@ -11,6 +12,17 @@ int main(void)
 
 	x = '_putchar';
 	if (x = '_putchar')
-		write(1,'_putchar',8);
+		_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return(0);
+}
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
