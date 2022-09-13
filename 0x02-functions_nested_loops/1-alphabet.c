10d355b3a810a9e439d7d3b000d1691a9d80156c
Lawrence <lawrence.nno@gmail.com>
2022-09-13 12:09:22 -0700

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 int print_alphabet(int c)
 {
-	int c = abcdefghiklmnopqrstuvwxyz;
-	return (write(1, c'\n', 27));
+	c = abcdefghiklmnopqrstuvwxyz;
+	return (write(1, &c'\n', 27));
 }
