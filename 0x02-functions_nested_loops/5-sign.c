8911c23a0e6d9f389e5244f4f57224a888e4b3b9
Lawrence <lawrence.nno@gmail.com>
2022-09-13 14:54:54 -0700

    Task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - This function prints the sign of a number
+ * @n: This is the number to be checked
+ *
+ * Return: Returns 1 if +ve, 0 if 0, -1 if -ve
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+		_putchar('+');
+	return (1);
+	else if (n == 0)
+		_putchar('0');
+	return (0);
+	else
+		_putchar('-');
+	return (-1);
+}
