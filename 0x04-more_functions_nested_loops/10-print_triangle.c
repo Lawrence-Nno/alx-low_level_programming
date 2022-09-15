338276458aa457f9a452222718859620d2a976f6
Lawrence <lawrence.nno@gmail.com>
2022-09-15 13:19:32 -0700

    TAsk10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_triangle - This function prints a pyramid.
+ * @size: Size of the pyramid
+ * Return: Nothing to return for now
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int lmt = size;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+			{
+				lmt--;
+				for (j = 0; j < size; j++)
+				{
+					if (j < lmt)
+					{
+						_putchar(32);
+					}
+					else
+					{
+						_putchar(35);
+																				}
+				}
+				_putchar('\n');
+			}
+		}
+		else
+		{
+		_putchar('\n');
+	}
+}
