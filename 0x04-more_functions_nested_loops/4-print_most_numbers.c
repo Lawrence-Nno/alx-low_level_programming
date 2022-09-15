6a4ae77a31bc6be5df88ae820980aa69760d51ca
Lawrence <lawrence.nno@gmail.com>
2022-09-15 06:17:57 -0700

    Task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 	int x;
 
 	for (x = '0'; x <= 9; x++)
+	{
 		if (x != '2' && x != '4')
-		{
-			_putchar(x);
-		}
-	_putchar('\n');
+			_putchar(x)
+	}
+	_putchar('\n')
 }
