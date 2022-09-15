0730e959c8abd626644a4efadf03ca69bc7f7b4b
Lawrence <lawrence.nno@gmail.com>
2022-09-15 04:43:37 -0700

    Task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _isupper(int c) - This function checks for upper case letters.
+ *
+ * @c is the input to be checked
+ * Return: Returns 1 if uppercase and 0 if lower case
+ */
+
+int _isupper(int c)
+{
+	if (c = A; c <= Z; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
