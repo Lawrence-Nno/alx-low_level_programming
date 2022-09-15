4fc3f4b4b76a01fa028a985fba1f9b028c25d64e
Lawrence <lawrence.nno@gmail.com>
2022-09-15 06:42:46 -0700

    Task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - This function prints numbers 0-14 ten times
+ * Return: No return for this function
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int n;
+
+	while (i < 10)
+	{
+		for (n = '0'; n <= '14'; n++)
+		{
+			if (n > '9')
+			{
+				_putchar('n' + n % 10);
+			}
+			else
+			{
+				_putchar('n');
+			}
+			_putchar('\n');
+		}
+	}
+}
