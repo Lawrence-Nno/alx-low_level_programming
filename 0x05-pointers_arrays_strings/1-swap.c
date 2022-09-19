7e1a91e1df19e9232f1c543ee6c9b84b680a5ac2
Lawrence <lawrence.nno@gmail.com>
2022-09-19 11:58:59 -0700

    Task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,6 @@
  * swap_int - This function swaps the value of two integers
  * @a: One of the integers to be swapped
  * @b: The other integer to be swapped
- * @c: Where the initial value of *a will be stored before swapping
  * Return: Nth for now
  */
 
