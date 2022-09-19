aea4a3b9502e5d88e7e27720bd4a1d0c24c09de0
Lawrence <lawrence.nno@gmail.com>
2022-09-19 13:10:09 -0700

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string
+ * @str: The string to be printed
+ * Return: Nothing for now
+ */
+
+void _puts(char *str)
+{
+	char str;
+	for (str[n] = str[0]; str[n] != '\0'; str[n]++)
+	{
+		_putchar((str[n]) + '0');
+	}
+}
