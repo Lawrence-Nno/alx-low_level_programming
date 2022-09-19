56ce58e594a5039b51f306610348047646e7135f
Lawrence <lawrence.nno@gmail.com>
2022-09-19 12:19:39 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - This function checks for the length of a string
+ * @s: The string whose length is to be checked
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+	for (n = 0; *s != '\0'; ++n)
+	return (n);
+}
