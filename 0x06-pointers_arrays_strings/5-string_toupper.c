79714d94cd9ecc0faf960298f1e81c068abfbfc9
Lawrence <lawrence.nno@gmail.com>
2022-09-21 16:02:31 -0700

    Task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,7 +10,7 @@ char *string_toupper(char *ch)
 {
 	int n = 0;
 
-	while(*(ch + n) != '\0')
+	while (*(ch + n) != '\0')
 	{
 		if (*(ch + n) >= 97 && *(ch + n) <= 122)
 			*(ch + n) = *(ch + n) - ' ';
