ceec745337cb3c95c0d30bea00f01799ac1d6630
Lawrence <lawrence.nno@gmail.com>
2022-09-23 05:09:25 -0700

    Task14

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -18,7 +18,7 @@ void print_buffer(char *b, int size)
 	if (size <= 0)
 	{
 		printf("\n");
-		continue;
+		return;
 	}
 	while (n < size)
 	{
@@ -52,3 +52,4 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		n += 10;
 	}
+}
