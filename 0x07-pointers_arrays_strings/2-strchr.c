e2cda4be187b3a21e827a54b4c8ac8c5e8bc89c6
Lawrence <lawrence.nno@gmail.com>
2022-09-26 07:24:06 -0700

    Task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
-		else {};
 	}
 	return ('\0');
 }
