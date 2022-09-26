04e04e5dfb82405c499490baf27e7f8dd0b4ddff
Lawrence <lawrence.nno@gmail.com>
2022-09-26 07:47:17 -0700

    task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,17 +15,17 @@ unsigned int _strspn(char *s, char *accept)
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
 			{
-				if (*s == accept[i])
-				{
-					b++;
-					break;
-				}
-				else if (accept[i + 1] == '\0')
-				{
-					return (b);
-				}
+				b++;
+				break;
 			}
+			else if (accept[i + 1] == '\0')
+			{
+				return (b);
+			}
+		}
 		s++;
 	}
 	return (b);
