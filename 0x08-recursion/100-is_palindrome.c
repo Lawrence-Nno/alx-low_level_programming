422834fa7857c17a3f5ec038123019c6a8a87d17
Lawrence <lawrence.nno@gmail.com>
2022-09-28 09:46:48 -0700

    Task100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_palindrome - This func identifies a palindrome
+ * @s: The string to identify
+ * Return: Returns 1 is positive
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,4 @@ int _sqrt_recursion(int n);
 int sqr(int a, int b);
 int is_prime_number(int n);
 int prime(int a, int b);
+int is_palindrome(char *s);
