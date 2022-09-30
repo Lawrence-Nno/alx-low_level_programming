965cc96070ea75c9a1b1d6661728775173571481
Lawrence <lawrence.nno@gmail.com>
2022-09-30 05:59:37 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,13 @@
 
 /**
  * main - This program prints its name
+ * @argc: The argument count
+ * @argv: The array of char holding the command line arguments
  * Return: Returns 0 on completion
  */
 
 int main(int argc, char* argv[])
 {
-	printf("%s\n", argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
