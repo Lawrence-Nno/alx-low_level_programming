47ea91c63cf4b296f4cf89800e6cefcb438bd19b
Lawrence <lawrence.nno@gmail.com>
2022-09-30 07:02:22 -0700

    Task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - This program multiplies two numbers
+ * @argc: The argument count
+ * @argv: The array of strings holding the arguments
+ * Return: Returns 0 if successful and 1 if there is error
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int mul;
+
+	a = argv[1];
+	b = argv[2];
+	mul = argv[1] * argv[2];
+	if (argc == 3)
+	{
+		printf("%d\n", mul)
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
