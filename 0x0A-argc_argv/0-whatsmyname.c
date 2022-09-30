a4a17cc4e52d6eb934edeaf47f348ce2d2336d2d
Lawrence <lawrence.nno@gmail.com>
2022-09-30 05:47:45 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints its name
+ * Return: Returns 0 on completion
+ */
+
+int main(int argc char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+I will update you ater, please bear with me
