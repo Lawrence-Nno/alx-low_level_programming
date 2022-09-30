ee63e6a6320c6d2d5d4d319c7ece38c65239a8a4
Lawrence <lawrence.nno@gmail.com>
2022-09-30 06:13:36 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * Return: Returns 0 on completion
  */
 
-int main(int __attribute__((__unused__)) argc, char* argv[])
+int main(int __attribute__((unused)) argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
