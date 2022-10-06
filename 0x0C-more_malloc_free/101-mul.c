be521a41c25e16204beab7cd1172bbfe6b2c0e54
Lawrence <lawrence.nno@gmail.com>
2022-10-06 07:54:49 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,28 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-int _isdigit(char *argv);
-
-/**
- * _isdigit - Checks if strings are digits
- * @argv: Contains the current argument
- * Return: Returns 0 if all strings are digits and 1 otherwise
- */
-
-int _isdigit(char *argv)
-{
-	int i;
-
-	i = 0;
-	while (argv[i])
-	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
-}
 
 /**
  * main - The main function
@@ -43,7 +21,7 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (_isdigit(argv[i]))
+		if (isdigit(argv[i]))
 		{
 			printf("Error\n");
 			exit(98);
