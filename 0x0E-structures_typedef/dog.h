a6eb7c88bdc0f712da10125d0c30b335d8d9e673
Lawrence <lawrence.nno@gmail.com>
2022-10-10 04:18:24 -0700

    TAsk 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,18 @@
+#include <stdio.h>
+#include <stlib.h>
+
 /**
  * struct dog - This struct is about the details of a dog.
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner's details
+ *
+ * Description: No long description needed
  */
 
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
