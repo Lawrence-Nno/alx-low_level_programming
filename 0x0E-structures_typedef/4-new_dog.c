2b6e72fa404150607077ce01158c3889b7228294
Lawrence <lawrence.nno@gmail.com>
2022-10-10 06:50:36 -0700

    TAsk 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -45,25 +45,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 		p->owner[c] = owner[c];
 	return (p);
 }
-/*
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
-}*/
