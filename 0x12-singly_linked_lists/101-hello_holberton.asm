5860a3241470d299939d7d9f1b962b583df91dd7
Lawrence <lawrence.nno@gmail.com>
2022-10-20 09:54:31 -0700

    Task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,24 @@
 ;Desc: A 64-bit assembly program that prints Hello, Holberton
 
 extern printf
+
 section .text
-global main
+	global main
 
 main:
-push rbp
+	push rbp
 
-mov rdi,fmt
-mov rsi,msg
-mov rax,0
-call printf
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
 
-pop rbp
+	pop rbp
 
-mov rax,0
-ret
+	mov rax,0
+	ret
 
 section .data
-msd: db "Hello, Holberton", 0
-fmt: db "%s", 10, 0
+
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
