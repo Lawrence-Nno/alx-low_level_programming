dc226d72a381529621bdd9c0925501f4b820720d
Lawrence <lawrence.nno@gmail.com>
2022-10-20 09:44:08 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - This func prints a string before main func is executed
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
