a24a5882dd46485bfb327e056c5720877c2b5e4b
Lawrence <lawrence.nno@gmail.com>
2022-10-27 08:17:03 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * flip_bits - This func returns the number of bits you would need to flip
- * 		to get from one number to another
+ * flip_bits - This func returns the number of bits you would need
+ * 		to flip to get from one number to another
  * @n: The number
  * @m: The number to flip to
- * Return: Returns the number to bits to flip
+ * Return: Returns the number of bits to flip
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
