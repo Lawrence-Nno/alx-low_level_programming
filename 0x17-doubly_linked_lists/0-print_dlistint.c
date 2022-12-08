2f08f849af6d4434980bf67348ca77e1a4b2e0a3
Lawrence <lawrence.nno@gmail.com>
2022-12-08 10:43:45 -0800

    Added 0-print_dlistint.c file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,6 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
+
         if (h == NULL)
                 return (0);
 
@@ -19,6 +20,8 @@ size_t print_dlistint(const dlistint_t *h)
                 printf("%d\n", h->n);
                 h = h->next;
         }
+
         printf("%d\n", h->n);
+
         return (count + 1);
 }
