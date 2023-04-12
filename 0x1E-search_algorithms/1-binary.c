12f036f0718eb3a71af3479a632cb0558e06a27a
Lawrence Nno <Lawrence.Nno@gmail.com>
2023-04-12 23:51:31 +0100

    Added TAsk 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 	for (left = 0, right = size - 1; right >= left;)
 	{
 		printf("Searching in array: ");
-		for (v = left; v < right; i++)
+		for (v = left; v < right; v++)
 			printf("%d, ", array[v]);
 		printf("%d\n", array[v]);
 
